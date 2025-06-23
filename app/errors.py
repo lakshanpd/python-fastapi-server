a9a9c1fa009b39ef6a8e6332ccafebe251986c26
lakshanpd <lakshanpd.cse21@gmail.com>
2025-06-23 17:33:54 +0530

    add errors module

---
+++ app/errors.py
@@ -0,0 +1,11 @@
+# Custom exception for errors related to database operations
+class DatabaseError(Exception):
+    pass
+
+# Custom exception for errors during user registration process
+class UserRegistrationError(Exception):
+    pass
+
+# Custom exception for invalid data type errors
+class InvalidTypeError(Exception):
+    pass
