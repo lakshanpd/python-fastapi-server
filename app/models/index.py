f6ed2120aa54aa6614b807d04e5a61f30b833d19
lakshanpd <lakshanpd.cse21@gmail.com>
2025-06-23 15:47:52 +0530

    add user model

---
+++ app/models/index.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from datetime import datetime, date
+
+class User(BaseModel):
+    id: int
+    first_name: str
+    last_name: str | None = None
+    birthday: date
+    email: str
+    phone_number: str | None = None
+    updated_at: datetime
+    created_at: datetime
+    
