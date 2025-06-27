a6f8239cdb9ff8df200c69da6520e88fabbbaeae
lakshanpd <lakshanpd.cse21@gmail.com>
2025-06-27 11:51:39 +0530

    refactor: use APIRouter for router management

---
+++ app/routes/auth.py
@@ -0,0 +1,53 @@
+from fastapi import APIRouter, HTTPException, status
+from models import User, LoginDetails, RefreshTokenRequest
+from auth import register_user, login_user, token_refresh
+from errors import DatabaseError, UserRegistrationError, UserLoginError
+import jwt
+import traceback
+import logging
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+router = APIRouter()
+
+@router.post("/register")
+async def register(user: User):
+    try:
+        register_user(user=user)
+        logging.info("user registered successfully")
+        return {"message": "user registered successfully", "status": status.HTTP_201_CREATED}
+    
+    except (DatabaseError, UserRegistrationError) as e:
+        logging.error("handled error: \n%s", traceback.format_exc())
+        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    
+@router.post("/login")
+async def login(loginDetails: LoginDetails):
+    try:
+        return login_user(loginDetails)
+    
+    except UserLoginError as e:
+        logging.error("handled error: \n%s", traceback.format_exc())
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+    
+    except (DatabaseError, UserRegistrationError) as e:
+        logging.error("handled error: \n%s", traceback.format_exc())
+        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    
+@router.post("/refresh-token")
+async def refresh_token(request: RefreshTokenRequest):
+    try:
+        return token_refresh(request)
+        
+    except (jwt.exceptions.InvalidIssuerError) as e:
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+    
+    except (jwt.exceptions.InvalidAudienceError) as e:
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+    
+    except UserLoginError as e:
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+
+    except (DatabaseError, UserRegistrationError) as e:
+        logging.error("handled error: \n%s", traceback.format_exc())
+        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))  
--- app/server.py
+++ app/server.py
@@ -1,57 +1,7 @@
-from fastapi import FastAPI, HTTPException, status
-from models import User, LoginDetails, RefreshTokenRequest
-from auth import login_user, token_refresh, register_user
-from errors import DatabaseError, UserRegistrationError, UserLoginError
-import logging
-import traceback
-import jwt
-
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+from fastapi import FastAPI
+from routes import auth
 
 app = FastAPI()
 
-@app.post("/register/")
-async def register(user: User):
-    try:
-        register_user(user=user)
-        logging.info("user registered successfully")
-        return {"message": "user registered successfully", "status": status.HTTP_201_CREATED}
-    
-    except (DatabaseError, UserRegistrationError) as e:
-        logging.error("handled error: \n%s", traceback.format_exc())
-        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-    
-@app.post("/login")
-async def login(loginDetails: LoginDetails):
-    try:
-        return login_user(loginDetails)
-    
-    except UserLoginError as e:
-        logging.error("handled error: \n%s", traceback.format_exc())
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
-    
-    except (DatabaseError, UserRegistrationError) as e:
-        logging.error("handled error: \n%s", traceback.format_exc())
-        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
-    
-@app.post("/refresh-token")
-async def refresh_token(request: RefreshTokenRequest):
-    try:
-        return token_refresh(request)
-        
-    except (jwt.exceptions.InvalidIssuerError) as e:
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
-    
-    except (jwt.exceptions.InvalidAudienceError) as e:
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
-    
-    except UserLoginError as e:
-        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
-
-    except (DatabaseError, UserRegistrationError) as e:
-        logging.error("handled error: \n%s", traceback.format_exc())
-        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))  
-    
-    
-    
+app.include_router(auth.router)
     
