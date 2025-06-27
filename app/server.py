441e680ba9c02b44e190d988fce3386a847deab8
lakshanpd <lakshanpd.cse21@gmail.com>
2025-06-27 08:23:19 +0530

    feat: create /login API

--- app/server.py
+++ app/server.py
@@ -1,7 +1,8 @@
 from fastapi import FastAPI, HTTPException, status
-from models import User
+from models import User, LoginDetails
 from auth.register import register_user
-from errors import DatabaseError, UserRegistrationError
+from auth.login   import login_user
+from errors import DatabaseError, UserRegistrationError, UserLoginError
 import logging
 import traceback
 
@@ -15,6 +16,20 @@ async def register(user: User):
         register_user(user=user)
         logging.info("user registered successfully")
         return {"message": "user registered successfully", "status": status.HTTP_201_CREATED}
+    
+    except (DatabaseError, UserRegistrationError) as e:
+        logging.error("handled error: \n%s", traceback.format_exc())
+        return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    
+@app.post("/login")
+async def login(loginDetails: LoginDetails):
+    try:
+        return login_user(loginDetails)
+    
+    except UserLoginError as e:
+        logging.error("handled error: \n%s", traceback.format_exc())
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=str(e))
+    
     except (DatabaseError, UserRegistrationError) as e:
         logging.error("handled error: \n%s", traceback.format_exc())
         return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
