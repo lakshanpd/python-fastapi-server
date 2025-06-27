72e155ccdba831a1138ce9ca9353b77d0dd45d87
lakshanpd <lakshanpd.cse21@gmail.com>
2025-06-27 09:03:24 +0530

    feat: add function for token refresh

--- app/auth/login.py
+++ app/auth/login.py
@@ -1,10 +1,11 @@
-from models import LoginDetails
+from models import LoginDetails, RefreshTokenRequest
 from database.db import get_hashed_password, get_user_claims
 import bcrypt
 import jwt
 import os
 from dotenv import load_dotenv
 from errors import UserLoginError
+from datetime import datetime, timezone
 
 load_dotenv()
 secret_key = os.getenv("secret_key")
@@ -30,4 +31,19 @@ def login_user(loginDetails: LoginDetails):
     else:
         raise UserLoginError("user credentials are invalid.")
     
-    
+def token_refresh(request: RefreshTokenRequest):
+    decoded_jwt = jwt.decode(request.refresh_token, secret_key, algorithms=["HS256"], audience="python-fastapi-server", issuer="python-fastapi-server")
+    if (decoded_jwt["exp"] > int(datetime.now(timezone.utc).timestamp())):
+        user_claims = get_user_claims(decoded_jwt["email"])
+        access_token = jwt.encode(
+            payload=user_claims[0],
+            key=secret_key,
+            algorithm="HS256"
+        )
+        return {
+            "access token": access_token
+        }
+    else:
+        raise UserLoginError("refresh token has been expired.")
+
+
--- app/models.py
+++ app/models.py
@@ -16,4 +16,7 @@ class User(BaseModel):
 class LoginDetails(BaseModel):
     email: str
     password: str
+
+class RefreshTokenRequest(BaseModel):
+    refresh_token: str
     
