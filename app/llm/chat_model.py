2418654bd583cff84ecf5e7c3a3d4062398f81ed
lakshanpd <lakshanpd.cse21@gmail.com>
2025-06-29 19:11:32 +0530

    feat: add function to delete user chat history

--- app/llm/chat_model.py
+++ app/llm/chat_model.py
@@ -2,16 +2,20 @@ from .model import model
 from redis_handler import redis_handler
 from langchain_core.messages import HumanMessage, SystemMessage
 
+history_handler = redis_handler()
+
 def chat_model(user_id, user_input):
     system_message = SystemMessage(content="You are a helpful AI bot knowing everything in cricket. Answer every question related to cricket in a short way.")
-    
-    history_handler = redis_handler()
     history = history_handler.get_messages(user_id, 5) 
-    
     user_message = HumanMessage(content=user_input)
     
     messages = [system_message] + history + [user_message]    
     response = model.invoke(messages)
+    
     history_handler.set_messages(user_id, [user_message, response])
 
     return response.content
+
+def clear_chat_history(user_id):
+    history_handler.delete_all(user_id)
+    
