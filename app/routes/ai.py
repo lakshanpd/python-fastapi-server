c386bafaa4acf9745391209d1400795b8360f4d3
lakshanpd <lakshanpd.cse21@gmail.com>
2025-06-29 15:52:16 +0530

    update: add authorization middle to /translate API

--- app/routes/ai.py
+++ app/routes/ai.py
@@ -1,18 +1,20 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Depends
 from models import TranslatorParams
 from llm.language_translator import language_translator
 import logging
 import traceback
+from auth import validate_token
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 router = APIRouter()
 
 @router.get("/translate")
-async def translate(language: str, text: str):
+async def translate(request = Depends(validate_token)):
     try:
-        params = TranslatorParams(language=language, text=text)
+        params = TranslatorParams(language=request["request"].query_params["language"], text=request["request"].query_params["text"])
         return language_translator(params=params)
     except Exception as e:
         logging.error("handled error: \n%s", traceback.format_exc())
         return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+    
