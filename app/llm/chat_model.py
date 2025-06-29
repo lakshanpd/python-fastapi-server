af8a59cca82665ce5f9f0d68769e71d27eb7ac2f
lakshanpd <lakshanpd.cse21@gmail.com>
2025-06-29 13:17:46 +0530

    feat: create chat model

---
+++ app/llm/chat_model.py
@@ -0,0 +1,17 @@
+from .model import model
+from app.redis_handler import redis_handler
+from langchain_core.messages import HumanMessage, SystemMessage
+
+def chat_model(user_id, user_input):
+    system_message = SystemMessage(content="You are a helpful AI bot knowing everything in cricket. Answer every question related to cricket in a short way.")
+    
+    history_handler = redis_handler()
+    history = history_handler.get_messages(user_id, 5) 
+    
+    user_message = HumanMessage(content=user_input)
+    
+    messages = [system_message] + history + [user_message]    
+    response = model.invoke(messages)
+    history_handler.set_messages(user_id, [user_message, response])
+
+    return response.content
