ca94bf29ad3a26b71564b291591a35631aaf5085
lakshanpd <lakshanpd.cse21@gmail.com>
2025-06-30 16:32:32 +0530

    feat: add sync to store function

--- app/chroma_handler.py
+++ app/chroma_handler.py
@@ -5,6 +5,8 @@ import os
 import getpass
 import asyncio
 from langchain_community.document_loaders import DirectoryLoader
+from langchain_chroma import Chroma
+from uuid import uuid4
 import chromadb
 
 load_dotenv()
@@ -13,10 +15,12 @@ if not os.environ.get("GOOGLE_API_KEY"):
   
 class ChromaHandler:
     def __init__(self):
-        embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
-        self.chunker = SemanticChunker(embedding)
-        self.chroma_client = chromadb.PersistentClient()
-
+        self.embedding = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+        self.chunker = SemanticChunker(self.embedding)
+        self.persistent_client = chromadb.PersistentClient()
+        
+    def get_collection_name(self, dir_path):
+        return os.path.splitext(os.path.basename(dir_path))[0]
         
     async def make_chunks(self, dir_path):
         loader = DirectoryLoader(dir_path, glob="*.txt")
@@ -24,26 +28,31 @@ class ChromaHandler:
 
         chunks_dict = {}
         for doc in docs:
-            file_name = doc.metadata["source"]
+            file_name = self.get_collection_name(doc.metadata["source"])
             chunks_dict[file_name] = []
 
         chunks = await self.chunker.atransform_documents(docs)
 
         for chunk in chunks:
-            file_name = chunk.metadata["source"]
+            file_name = self.get_collection_name(chunk.metadata["source"])
             chunks_dict[file_name].append(chunk)
 
         return chunks_dict
-
     
-    # def add_to_store(self, dir_path):
-    #     collection_names = [f.split(".")[0] for f in os.listdir(dir_path) if f.endswith(".txt")]
-    #     chunks = self.make_chunks(dir_path)
-    #     for collection_name in collection_names:
-    #         collection = self.chroma_client.get_or_create_collection(collection_name)
+    async def sync_to_store(self, dir_path):
+        chunks_dict = await self.make_chunks(dir_path)
+        existing_collections = [col.name for col in self.persistent_client.list_collections()]
+
+        for collection_name in list(chunks_dict.keys()):
+            if (collection_name in existing_collections):
+                self.persistent_client.delete_collection(collection_name)  
+                          
+            vector_store = Chroma(
+                collection_name=collection_name,
+                embedding_function=self.embedding,
+                client=self.persistent_client,  
+            )
             
-if __name__ == "__main__":
-    test_obj = ChromaHandler()
-    result = asyncio.run(test_obj.make_chunks("./data/"))
-    print(result)
-    # test_obj.make_collections("./data/")
+            uuids = [str(uuid4()) for _ in range(len(chunks_dict[collection_name]))]
+            vector_store.add_documents(documents=chunks_dict[collection_name], ids=uuids) 
+                 
