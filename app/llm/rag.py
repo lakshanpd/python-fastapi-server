2d6d36256ac91e7aef97e25c31a1336fb84ea1cd
lakshanpd <lakshanpd.cse21@gmail.com>
2025-06-30 21:12:58 +0530

    feat: create rag model function

---
+++ app/llm/rag.py
@@ -0,0 +1,32 @@
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from redis_handler import redis_handler
+from chroma_handler import chroma_handler
+from .model import model
+from langchain_core.messages import HumanMessage
+
+def rag_model(user_id, user_query):
+    system_message = "You are a helpful AI bot knowing upcoming cricket tournament. Also, you can answer general questions about cricket. If user asks about the upcoming cricket tournament, always use retrieved data. If you don't have enough data, tell that you don't know about it. Give the answers directly and politely."
+    user_message = HumanMessage(content=user_query)
+    
+    template = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_message),
+            MessagesPlaceholder("history"),
+            ("system", "Here are retrieved information: {retrieved_info}"),
+            ("human", "Answer to this question: {question}")
+        ]
+    )
+    
+    history_handler = redis_handler()
+    history = history_handler.get_messages(user_id, 5)
+    
+    prompt = template.invoke({
+        "history": history,
+        "retrieved_info": chroma_handler.query_similar_docs(user_query),
+        "question": user_query
+     })
+            
+    response = model.invoke(prompt)
+    history_handler.set_messages(user_id, [user_message, response])
+    
+    return response.content
