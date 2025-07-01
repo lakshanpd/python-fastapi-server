efafabb6a6decae04a5d7502dd4955bfea1e4bcd
lakshanpd <lakshanpd.cse21@gmail.com>
2025-07-01 07:21:30 +0530

    refactor: replace sql connection and functions from a SqlHandler class

--- app/database/sql_handler.py
+++ app/database/sql_handler.py
@@ -1,54 +1,59 @@
+from dotenv import load_dotenv
+import os
+import mysql.connector
 from models import User
-from .connection import connect_to_mysql
 from .queries import add_user_query, get_email_query, get_password_query, get_user_claims_query
 from errors import DatabaseError
 from datetime import datetime, timezone
 
-def add_user_to_db(user: User):
-    connection = connect_to_mysql()
-    if connection:
+load_dotenv()
+
+config = {
+  'user': os.getenv("user"),
+  'password': os.getenv("password"),
+  'host': os.getenv("host"),
+  'database': os.getenv("database")
+}
+
+class SqlHandler:
+    def __init__(self):
+        self.connection = mysql.connector.connect(**config)
+        self.cursor = self.connection.cursor()
+    
+    def add_user_to_db(self, user: User):
         try:
-            cursor = connection.cursor()
+            cursor = self.connection.cursor()
             user_data = (str(user.id), user.first_name, user.last_name, user.birthday, user.email, user.phone_number, user.updated_at, user.created_at, user.password)
             cursor.execute(add_user_query, user_data)
-            connection.commit()
-        except Exception as error:
+            self.connection.commit()
+        except Exception:
             raise DatabaseError("Error while adding user to db")
-          
-# if a record with the given email exists returns true, otherwise false  
-def check_email_exist(email: str):
-    connection = connect_to_mysql()
-    if connection:
+            
+    # if a record with the given email exists returns true, otherwise false  
+    def check_email_exist(self, email: str):
         try:
-            cursor = connection.cursor()
-            cursor.execute(get_email_query, (email,))
-            result = cursor.fetchall()
+            self.cursor.execute(get_email_query, (email,))
+            result = self.cursor.fetchall()
             if len(result) > 0:
                 return True 
             return False
-        except Exception as error:
+        except Exception:
             raise DatabaseError("Error while adding user to db")
-      
-# get hashed password from database for given email  
-def get_hashed_password(email):
-    connection = connect_to_mysql()
-    if connection:
+        
+    # get hashed password from database for given email  
+    def get_hashed_password(self, email):
         try:
-            cursor = connection.cursor()
-            cursor.execute(get_password_query, (email,))
-            result = cursor.fetchone()
+            self.cursor.execute(get_password_query, (email,))
+            result = self.cursor.fetchone()
             return result[0]
-        except Exception as error:
+        except Exception:
             raise DatabaseError("Error while fetching user password to match")
         
-# get user claims from database for given email  
-def get_user_claims(email):
-    connection = connect_to_mysql()
-    if connection:
+    # get user claims from database for given email  
+    def get_user_claims(self, email):
         try:
-            cursor = connection.cursor()
-            cursor.execute(get_user_claims_query, (email,))
-            result = cursor.fetchone()
+            self.cursor.execute(get_user_claims_query, (email,))
+            result = self.cursor.fetchone()
             access_token_claims = {
                 "iss": "python-fastapi-server",
                 "aud": "python-fastapi-server",
@@ -69,7 +74,7 @@ def get_user_claims(email):
                 "exp": int(datetime.now(timezone.utc).timestamp()) + 604800 # expiration time is one week
             }
             return [access_token_claims, refresh_token_claims]
-        except Exception as error:
+        except Exception:
             raise DatabaseError("Error while fetching user claims")
         
-        
+sql_handler = SqlHandler()
